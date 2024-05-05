5525716e1d393d1fa4bc5e53f2927149db88ed5c
Tristan Kraft <harsifpv@gmail.com>
2024-05-05 16:00:09 +0200

    corrected time and session duration logging

--- backend/api.py
+++ backend/api.py
@@ -4,34 +4,29 @@ from pymongo import MongoClient
 from datetime import datetime
 
 app = Flask(__name__)
-CORS(app)  # Active CORS pour toutes les routes de l'application
+CORS(app)
 
 # Connexion à la base de données MongoDB
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://localhost:27017/')
 db = client['gym_entry']
 collection = db['entry']
 
-
-# Endpoint pour enregistrer une entrée
 @app.route('/api/entry', methods=['POST'])
 def add_entry():
-    data = request.json
-    if 'member_id' not in data:
-        return jsonify({'error': 'Missing member_id'}), 400
+    data = request.get_json()
+    member_id = data['member_id']
+    entry_time = data['entry_time']
+    session_duration = data['session_duration']
+
+    # Ajouter une nouvelle entrée dans la base de données
     entry = {
-        'member_id': data['member_id'],
-        'entry_time': datetime.now()
+        'member_id': member_id,
+        'entry_time': entry_time,
+        'session_duration': session_duration
     }
-    result = collection.insert_one(entry)
-    return jsonify({'message': 'Entry added successfully', 'entry_id': str(result.inserted_id)}), 201
+    collection.insert_one(entry)
 
-
-# Endpoint pour récupérer toutes les entrées
-@app.route('/api/entries', methods=['GET'])
-def get_entries():
-    entries = list(collection.find({}, {'_id': 0}))  # Exclure l'ID MongoDB
-    return jsonify(entries), 200
-
+    return jsonify({'message': 'Enregistrement ajouté avec succès !'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
